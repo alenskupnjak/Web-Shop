a0afde8d14025a0de042ce310d4b0ffb2849da33
Alen Skupnjak <alenskupnjak@yahoo.com>
2023-09-26 21:02:40 +0200

    Dodani modeli

---
+++ Movies/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Movies.Models
+{
+	public class Category
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[Required]
+		// definiramo da je minimalna duzina 2, a maksimalna 200 karaktera
+		[StringLength(200, MinimumLength = 2)]
+		public string Title { get; set; }
+
+		[ForeignKey("CategoryId")]
+		public virtual ICollection<ProductCategory> ProductCategories { get; set; }
+	}
+}
---
+++ Movies/Models/Product.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Movies.Models
+{
+	public class Product
+	{
+		[Key]
+		public int Id { get; set; }
+
+
+		[Required]
+		[StringLength(200, MinimumLength = 2)]
+		public string Title { get; set; }
+
+
+		public string Description { get; set; }
+
+
+		[Required]
+		// definiramo quantity da je decimalni broj sa 9 znamenki, od toga 2 iza decimalne tocke
+		[Column(TypeName = "decimal(9,2)")]
+		public decimal Quantity { get; set; }
+
+		[Required]
+		[Column(TypeName = "decimal(9,2)")]
+		public decimal Price { get; set; }
+
+
+		[ForeignKey("ProductId")]
+		public virtual ICollection<ProductCategory> ProductCategories { get; set; }
+
+
+		[ForeignKey("ProductId")]
+		public virtual ICollection<OrderItem> OrderItems { get; set; }
+
+
+		[ForeignKey("ProductId")]
+		public virtual ICollection<ProductImage> ProductImages { get; set; }
+
+
+	}
+}
