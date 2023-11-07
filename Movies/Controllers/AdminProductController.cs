de1b262ec69bc69751335621471cdc2258feeb81
Alen Skupnjak <alenskupnjak@yahoo.com>
2023-11-07 20:43:19 +0100

    Dodan API controler

--- Movies/Controllers/AdminProductController.cs
+++ Movies/Controllers/AdminProductController.cs
@@ -58,8 +58,6 @@ namespace Movies.Controllers
 		}
 
 		// POST: AdminProduct/Create
-		// To protect from overposting attacks, enable the specific properties you want to bind to.
-		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
 		[HttpPost]
 		[ValidateAntiForgeryToken]
 		public async Task<IActionResult> Create([Bind("Id,Title,Description,Active,Quantity,Price")] Product product)
--- Movies/Controllers/AdminUserController.cs
+++ Movies/Controllers/AdminUserController.cs
@@ -20,6 +20,8 @@ namespace Movies.Controllers
 		public async Task<IActionResult> Index()
 		{
 			var users = await _context.AppUser.ToListAsync();
+			// kako dobiti role za svakog usera
+			var roles = await _context.Roles.ToListAsync();
 			return View(users);
 		}
 
@@ -27,6 +29,22 @@ namespace Movies.Controllers
 		public async Task<IActionResult> Users()
 		{
 			return View();
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Edit(string id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+
+			var user = await _context.AppUser.FindAsync(id);
+			if (user == null)
+			{
+				return NotFound();
+			}
+			return View(user);
 		}
 	}
 }
